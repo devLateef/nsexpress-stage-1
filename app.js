440ded6af3988f338016d6aead48616d908b9839
devLateef <lateefolatayo@gmail.com>
2023-12-17 09:59:27 +0100

    Added more input fields

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 2000;
+const port = 8000;
 
 app.use(express.static('public'));
 app.use(express.json());
